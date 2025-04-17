349d46360cc42760d541cd2d55a9a272d98a1923
Ganesh Bharti <ganeshkundlikbharti728@gmail.com>
2025-04-17 20:32:32 +0530

    warpper classes in collection

---
+++ Collections/src/WrapperClasses/pkg/DemoWrapperClasses.java
@@ -0,0 +1,21 @@
+package WrapperClasses.pkg;
+
+public class DemoWrapperClasses {
+	
+	public static void main(String[] args) {
+		
+		int num = 100;
+		
+		Integer num1 = num;
+		
+		System.out.println(num1);
+		
+		
+		char ch = 'b';
+		
+		Character ch1 = ch ;
+		
+		System.out.println(ch1);
+	}
+
+}
